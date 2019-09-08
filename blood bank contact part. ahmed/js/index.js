b2425c8b137b623e007387253e46d36cfbc095dc
Your Name <you@example.com>
2019-09-08 15:16:34 +0200

    ahmed update

---
+++ blood bank contact part. ahmed/js/index.js
@@ -0,0 +1,24 @@
+
+
+//$(".test").hide(2000)
+
+
+// $("#btn").click(function(){
+//     $(".test").slideDown(2000, function(){
+//         $(".test").slideUp(2000, function(){
+//             $("#btn").css("background-color", "black")
+//         })
+//     });
+    
+// })
+// $("#btn").click(function(){
+//     $("header").animate({width:'100%',height:'400px'},2000)
+//     $("header").slideUp(2000)
+// })
+
+// $(".welcome").click(function(){
+//     alert($(".welcome").css("backgroundColor"))
+// })
+
+$("header").children().css("border","solid 1px green")
+
